76d4643da578489b830516c325c7eec64dd4582a
Leothurm <leonardo.thurm@gmail.com>
2023-01-18 13:33:11 -0300

    acessibilidade

--- script.js
+++ script.js
@@ -7,11 +7,19 @@ const btnMobile = document.getElementById('btn-mobile');
 function toggleMenu(event){
     if(event.type === 'touchstart') event.preventDefault();
     const nav = document.getElementById('nav');
-    nav.classList.toggle('active')
+    nav.classList.toggle('active');
 
+    // a constante abaixo faz verificar se possui uma classe active e retorna true ou false
+    const active = nav.classList.contains('active')
+    event.currentTarget.setAttribute('aria-expanded', active);
 
+    if(active) {
+        event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+    } else{
+        event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+
+    }
 }
-
 
 //eventos
 btnMobile.addEventListener('click', toggleMenu)
